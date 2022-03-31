1e2889a95eaeea693e044bd18509ce358bbc6b78
Matěj Šinogl <sinoglmatej@gmail.com>
2022-03-31 22:38:20 +0200

    Initial setup for basic backend

--- backend/api/reviews.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import ReviewsController from "./reviews.controller.js";
-
-const router = express.Router();
-
-router.get("/", ReviewsController.apiGetReviews);
-
-export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,35 @@
-import express from "express";
-import reviews from "./api/reviews.route.js";
+// External module imports
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
+// Get env variables if running in dev
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+
+// Internal module imports
+// Routes (Controllers)
+const indexRouter = require("./routes/index.js");
+
+// Creating the express server
 const app = express();
 
-app.use(express.json());
+// Connecting to the DB
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
-app.use("/api/v1/reviews", reviews);
-app.use("*", (req, res) => res.status(404).json({error: "not found"}));
+// Middleware
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+app.use("/", indexRouter);
 
-export default app;
+
+app.listen(process.env.PORT || 3000);
